2cfebd28c16931a37f718f24757e5ee6c4f245cd
seohyun9672 <ajeong4@my.bcit.ca>
2023-02-02 23:31:00 -0800

    add layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 import '../styles/variables.css'
+import Layout from '../comps/Layout/Layout'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Layout>
+    <Component {...pageProps} />
+  </Layout>
 }
